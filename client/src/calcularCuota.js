149d08d2ad1c02b068a002123b020a36d65f1d4b
Sofia Rios <sofia.rios@usm.cl>
2023-11-17 22:11:01 -0300

    añadida formula para calcular cuota

--- client/src/calcularCuota.js
+++ client/src/calcularCuota.js
@@ -5,10 +5,11 @@ const calcularCuota = (valorCredito, tasaMensual, plazo) => {
 
   //Pasar de pesos a UF
   valorCredito = valorCredito * valorUF;
-  tasaMensual = tasaMensual * valorUF;
+  tasaMensual = tasaMensual/100;
+  console.log(tasaMensual)
 
   //Calcular la cuota
-  var cuota = valorCredito / (1 - ((1 + tasaMensual)**(-plazo)) / tasaMensual );
+  var cuota = valorCredito / ((1 - ((1 + tasaMensual)**(-plazo))) / tasaMensual );
 
   return (cuota);
 
